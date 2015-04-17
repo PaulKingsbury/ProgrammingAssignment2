3bca5b481a38ed10fb5ea0193feaf3f367ef6637
PaulKingsbury <pkingsbury@gmail.com>
2015-04-17 13:52:09 -0700

    Completed programming assignment, uploading to github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## creates a holder object for cached matrices.  
+## Object holds both the matrix and functions for solving/inverting as well as returning the cached matrices
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL # initialize m to NULL; this should get overridden the first time makeCache is invoked
+    set <- function(y) { # this re-initializes the cache to NULL
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x # returns the original matrix
+    setInverse <- function(solve) m <<- solve # passes m through to built-in solve function
+    getInverse <- function() m # returns inverted matrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) # set defaults to function
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## solve the matrix
+cacheSolve <- function(x, ...) { # requires input of makeCacheMatrix object
+    ## Return a matrix that is the inverse of 'x', using a cached version if it is available
+    m <- x$getInverse() # get the cached inversion within x 
+    if (!is.null(m)) { #is the cache NULL or solved?  If cached, return the cache, otherwise solve and save to cache
+        message("using cached inversion") # hey look, it's a debugging function!
+        return(m) # I thought R didn't use explicit return statements?  Is this only to prevent further execution?
+    }
+    data <- x$get() # grab the content of the input matrix
+    m <- solve(data, ...) # finally, we invert the matrix
+    x$setInverse(m) # fill the cache with the inverted matrix
+    m # return the inverted matrix also
 }
